0b666db96957658d6261f87f2c151d0c6392ef14
Vanderson Vauruk <vandersonvauruk@Mac.lan>
2025-09-04 16:19:22 +0100

    add action bar in botton screen

--- src/views/CatTinderScreen.tsx
+++ src/views/CatTinderScreen.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, Image, ActivityIndicator } from 'react-native';
+import {
+  View,
+  FlatList,
+  Image,
+  ActivityIndicator,
+  TouchableHighlight,
+} from 'react-native';
 import { styles } from './styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCats } from '../store/catSlice';
 import { RootState } from '../store/store';
 import CatDetailCard from '../components/CatDetailCard';
+import Row from '../components/FlexBox/Row';
+import Col from '../components/FlexBox/Col';
 
 export const CatTinderScreen = () => {
   const dispatch = useDispatch();
@@ -12,7 +20,7 @@ export const CatTinderScreen = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   useEffect(() => {
-    dispatch(fetchCats(10) as any);
+    dispatch(fetchCats(100) as any);
   }, [dispatch]);
 
   if (loading) {
@@ -23,31 +31,74 @@ export const CatTinderScreen = () => {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={cats}
-        keyExtractor={item => item.id}
-        renderItem={({ item, index }) => (
-          <View style={styles.card}>
-            <Image
-              source={{ uri: item.url }}
-              style={styles.image}
-              resizeMode="cover"
-            />
-            {selectedIndex === index && <CatDetailCard id={item.id} />}
-          </View>
-        )}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        onMomentumScrollEnd={e => {
-          const idx = Math.round(
-            e.nativeEvent.contentOffset.x /
-              e.nativeEvent.layoutMeasurement.width,
-          );
-          setSelectedIndex(idx);
-        }}
-        extraData={selectedIndex}
-      />
+      <View style={{ height: '60%' }}>
+        <FlatList
+          data={cats}
+          keyExtractor={item => item.id}
+          renderItem={({ item, index }) => (
+            <View style={styles.card}>
+              <Image
+                source={{ uri: item.url }}
+                style={styles.image}
+                resizeMode="cover"
+              />
+              {selectedIndex === index && <CatDetailCard id={item.id} />}
+            </View>
+          )}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onMomentumScrollEnd={e => {
+            const idx = Math.round(
+              e.nativeEvent.contentOffset.x /
+                e.nativeEvent.layoutMeasurement.width,
+            );
+            setSelectedIndex(idx);
+          }}
+          extraData={selectedIndex}
+        />
+      </View>
+      <View style={styles.buttonBar}>
+        <Row>
+          <Col flex={0.5}>
+            <TouchableHighlight
+              style={styles.actionButton}
+              onPress={() => {}}
+              underlayColor={'#8d7373ff'}
+            >
+              <Image source={require('../assets/hate.png')} />
+            </TouchableHighlight>
+          </Col>
+          <Col flex={0.5}>
+            <TouchableHighlight
+              style={styles.actionButton}
+              onPress={() => {}}
+              underlayColor={'#8d7373ff'}
+            >
+              <Image source={require('../assets/like.png')} />
+            </TouchableHighlight>
+          </Col>
+        </Row>
+      </View>
+      <View style={styles.actionBar}>
+        <Row>
+          <Col flex={0.33}>
+            <TouchableHighlight onPress={() => {}} underlayColor={'#8d7373ff'}>
+              <Image source={require('../assets/powcat.png')} />
+            </TouchableHighlight>
+          </Col>
+          <Col flex={0.33}>
+            <TouchableHighlight onPress={() => {}} underlayColor={'#8d7373ff'}>
+              <Image source={require('../assets/message-circle.png')} />
+            </TouchableHighlight>
+          </Col>
+          <Col flex={0.33}>
+            <TouchableHighlight onPress={() => {}} underlayColor={'#8d7373ff'}>
+              <Image source={require('../assets/user.png')} />
+            </TouchableHighlight>
+          </Col>
+        </Row>
+      </View>
     </View>
   );
 };
